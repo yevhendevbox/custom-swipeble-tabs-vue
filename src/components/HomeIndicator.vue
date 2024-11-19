832a08e5984c6ed5a8268d1bdd0ab6739e6c5afc
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-11-19 11:55:37 +0200

    ref: update calculation on resize tabse

--- src/components/HomeIndicator.vue
+++ src/components/HomeIndicator.vue
@@ -104,6 +104,8 @@ function initTabs() {
   if (!indicatorRef.value) return
   if (!tabsRef.value) return
 
+  lefts.value = [];
+
   Array.from(tabsRef.value.children).forEach((tab, i) => {
     const tabWidth = _css(tab as HTMLElement, 'width')
 
@@ -121,13 +123,7 @@ function initTabs() {
   })
 
   indicatorSpace.value = lefts.value[1] - lefts.value[0]
-  // console.log('intiTab resize toggle', indicatorSpace.value)
 
-  // if (lefts.value.length > 1) {
-  //   indicatorSpace.value = lefts.value[1] - lefts.value[0]
-  // } else {
-  //   indicatorSpace.value = 0;
-  // }
   updateIndicatorPosition(props.index, 300)
 }
 
